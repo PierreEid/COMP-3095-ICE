dc1986dbdf466f10d35f9dd5e03f9d12caacbe99
Pierre <pierre.eid2@gmail.com>
2025-10-02 18:13:22 -0400

    Created project using spring initializer in intellij: Gradle - Kotlin

---
+++ microservice-parent/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ca.gbc.comp3095"
+version = "0.0.1-SNAPSHOT"
+description = "microservice-parent"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ microservice-parent/src/test/java/ca/gbc/comp3095/microserviceparent/MicroserviceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.microserviceparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
